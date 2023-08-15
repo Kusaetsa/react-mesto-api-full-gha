2818b2eb2609f879215bc0e5958d72514bb783ba
Julia Alexeeva <feyulpa@yandex.ru>
2023-08-15 19:39:31 +0300

    test2

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
